acabfcddeab89c486c95d4d9b81ca0f8261d024a
alexus <afidelo@protonmail.com>
2020-07-26 00:03:13 +0200

    might be all right for proof of concept; will be polished;

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.5.0;
 import { ERC20 } from "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC20 } from "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/upgrades/contracts/Initializable.sol";
+import "../node_modules/@openzeppelin/contracts/math/Math.sol";
 import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
-
 
 // I need to understand how to dynamically calculate dailylimit everytime;
 contract Controller is Initializable {
@@ -13,10 +13,19 @@ contract Controller is Initializable {
 
   address public parent;
   address public child;
-  uint256 public dailyLimit;
+  uint256 public dailyLimitGeneral;
+  uint256 public dailyLimitConcrete;
+  uint256 public spentSoFar;
+  // uint256 constant now;
+  // uint256 constant 1 days;
+  uint256 dayBegining;
+  uint256 dayEnding;
   IERC20 public tokenInstance;
-  event DailyLimitChanged(uint256 previousLimit, uint256 newLimit);
-  event SpendingApproved(address childAddress, uint256 amount);
+  bool frozen;
+  event DailyLimitGeneralChanged(uint256 previousLimit, uint256 newLimit);
+  event DailyLimitConcreteChanged(uint256 previousLimit, uint256 newLimit);
+  event AdditionalSpendingForTodayApproved(address childAddress, uint256 amount);
+  event SpendingHappend(address from, address to, uint256 balance);
 
   modifier isParent(){
     require(
@@ -37,23 +46,35 @@ contract Controller is Initializable {
       tokenInstance = _token;
   }
   function() external payable{}
-  function changeDailyLimit(uint256 _newDailyLimit) public isParent {
+
+  function startNewDay(){
+    uint256 newDayBegining = dayEnding + 1 minutes;
+    uint256 newDayEnding = dayEnding + 1 days;
+    dayBegining = newDayBegining;
+    dayEnding = newDayEnding;
+  }
+  function changeDailyLimitGeneral(uint256 _newDailyLimit) public isParent {
       require(_newDailyLimit !< 0, 'No need to set negative limit; 0 is enough');
       uint256 previousLimit = dailyLimit;
       dailyLimit = _newDailyLimit;
       emit DailyLimitChanged(previousLimit, _newDailyLimit);
   }
-  // there is no need for rejection of additional spending;
-  // this needs to be modified -> if resources are on Controller.sol 
-  function approveAdditionalSpending(uint256 amount) public isParent return (bool){
-      bool success = tokenInstance.approve(child, amount);
-      emit SpendingApproved(child, amount);
-      return sucess
-  }
-  // spend with idea that resources are on Controller
-  function spend(uint256 amount, address destination) public isChild{
+  function approveAdditionalSpendingForToday(uint256 amount) public isParent return (bool) {
       uint256 balance = tokenInstance.balanceOf(address(this));
+      require(amount != 0, 'Null not allowed');
+      require(balance > amount, 'You approved additional spending for today, but there is not enough balance for `spend` to occur');
+      dailyLimitConcrete.add(amount);
+      event AdditionalSpendingForTodayApproved(address childAddress, uint256 amount);
+  }
+  function spend(uint256 amount, address destination) public isChild, isParent {
+      uint256 balance = tokenInstance.balanceOf(address(this));
+      require(balance >= amount , 'Not enough balance');
+      require(spentSoFar <= dailyLimitConcrete, 'Approval of additional resources should be requested');
       bool success = tokenInstance.safeTransfer(destination, balance);
+      if(success){
+        emit SpendingHappend(address(this), destination, balance);
+      }
+      else revert;
   }
 
 }
