6e81045f7ffc7fa40c3b5cf48b017a2167bece0d
alexus <afidelo@protonmail.com>
2020-07-25 18:07:24 +0200

    adding CustomERC20.sol; will test when i configure `network.js`

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,10 +1,25 @@
-pragma solidity ^0.6.9;
+pragma solidity ^0.6.0;
 
-contract Controller {
+import "../node_modules/@openzeppelin/upgrades/contracts/Initializable.sol";
+import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 
-  address public value;
+contract Controller is Initializable {
 
+  using SafeMath for uint256;
+
+  address public parent;
+  address public child;
+  uint256 public dailyLimit;
+
+  function initialize(address _parent, address _child, uint256 _dailyLimit) public initializer {
+      parent = _parent;
+      child = _child;
+      dailyLimit = _dailyLimit;
+  }
+  function changeDailyLimit(uint256 _newDailyLimit) public returns (bool){
+      dailyLimit = _newDailyLimit;
+  }
   function increase(uint256 amount) public {
-    value += amount;
+      value += amount;
   }
 }
---
+++ contracts/CustomERC20.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.4.24 <0.7.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/upgrades/contracts/Initializable.sol";
+
+contract CustomERC20 is ERC20, Initializable{
+    string public version;
+    string private name_;
+    string private symbol_;
+    uint256 public MOCK_SUPPLY;
+
+  function initialize(string memory name, string memory symbol, address payable owner) public initializer {
+    version = "0.0.1";
+    name_ = name;
+    symbol_ = symbol;
+    MOCK_SUPPLY = 100e5;
+    _mint(owner, MOCK_SUPPLY);
+  }
+
+}
