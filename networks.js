b0727c998bbd6ce39bc689f76d8493e252eb8ad9
alexus <afidelo@protonmail.com>
2020-07-25 18:22:13 +0200

    configured;

--- networks.js
+++ networks.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const infuraProjectId = process.env.INFURA_PROJECT_ID;
+
 module.exports = {
   networks: {
     development: {
@@ -7,6 +11,14 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5e9,
       networkId: '*',
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, "https://ropsten.infura.io/v3/" + infuraProjectId, 0, 2),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false
     },
   },
 };
