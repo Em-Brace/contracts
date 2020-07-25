634a509446f7e1cf6f67d91cdbcfdcd1ad9ea148
alexus <afidelo@protonmail.com>
2020-07-25 18:24:14 +0200

    token finished; will deploy in order to test;

--- contracts/CustomERC20.sol
+++ contracts/CustomERC20.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.24 <0.7.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ERC20 } from "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/upgrades/contracts/Initializable.sol";
 
 contract CustomERC20 is ERC20, Initializable{
