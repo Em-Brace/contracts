84232d478a14c71e77f7091e6102e3f8cdc3343a
alexus <afidelo@protonmail.com>
2020-07-26 21:56:28 +0200

    added function to clear stale tranasctions -> should be called before spending;

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -9,49 +9,53 @@ import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 // I need to understand how to dynamically calculate dailylimit everytime;
 contract Controller is Initializable {
 
+  struct Transaction {
+    uint256 timestamp;
+    uint256 amount;
+  }
+
   using SafeMath for uint256;
 
   address public parent;
   address public child;
-  uint256 public dailyLimitGeneral;
-  uint256 public dailyLimitConcrete;
-  uint256 public spentSoFar;
-  // uint256 constant now;
-  // uint256 constant 1 days;
-  uint256 dayBegining;
-  uint256 dayEnding;
+  uint256 public _dailyLimit;
+  uint256 beginning;
+  Transaction[] public transactions;
   IERC20 public tokenInstance;
-  bool frozen;
-  event DailyLimitGeneralChanged(uint256 previousLimit, uint256 newLimit);
-  event DailyLimitConcreteChanged(uint256 previousLimit, uint256 newLimit);
+  event DailyLimitChanged(uint256 previousLimit, uint256 newLimit);
   event AdditionalSpendingForTodayApproved(address childAddress, uint256 amount);
   event SpendingHappend(address from, address to, uint256 balance);
 
   modifier isParent(){
     require(
-      msg.sender == parent, "Only parents can perform this operation");
-      _;
-    )
+      msg.sender == parent, "Only parents can perform this operation"
+    );
+    _;
   }
   modifier isChild(){
     require(
-      msg.sender == child, "Only child can perform this operation");
-      _;
-    )
+      msg.sender == child, "Only child can perform this operation"
+    );
+    _;
   }
-  function initialize(address _parent, address _child, uint256 _dailyLimit, address _token) public initializer {
+  // modifier canSpend(){
+  //   require(
+
+  //   )
+  // }
+  function initialize(address _parent, address _child, uint256 _dailyLimit, address _token, uint256 _beginning) public initializer {
       parent = _parent;
       child = _child;
       dailyLimit = _dailyLimit;
       tokenInstance = _token;
+      beginning = _beginning;
   }
   function() external payable{}
 
-  function startNewDay(){
-    uint256 newDayBegining = dayEnding + 1 minutes;
-    uint256 newDayEnding = dayEnding + 1 days;
-    dayBegining = newDayBegining;
-    dayEnding = newDayEnding;
+  function clearStaleTransactions(uint 256 beginning, uint256 ending){
+    for(uint256 i = 0; i < transactions.length; i++){
+      if(transactions[i].timestamp >= beginning && transactions[i].timestamp <= ending) delete transactions[i];
+    }
   }
   function changeDailyLimitGeneral(uint256 _newDailyLimit) public isParent {
       require(_newDailyLimit !< 0, 'No need to set negative limit; 0 is enough');
@@ -59,22 +63,27 @@ contract Controller is Initializable {
       dailyLimit = _newDailyLimit;
       emit DailyLimitChanged(previousLimit, _newDailyLimit);
   }
-  function approveAdditionalSpendingForToday(uint256 amount) public isParent return (bool) {
-      uint256 balance = tokenInstance.balanceOf(address(this));
-      require(amount != 0, 'Null not allowed');
-      require(balance > amount, 'You approved additional spending for today, but there is not enough balance for `spend` to occur');
-      dailyLimitConcrete.add(amount);
-      event AdditionalSpendingForTodayApproved(address childAddress, uint256 amount);
-  }
+  // function approveAdditionalSpendingForToday(uint256 amount) public isParent return (bool) {
+  //     uint256 balance = tokenInstance.balanceOf(address(this));
+  //     require(amount != 0, 'Null not allowed');
+  //     require(balance > amount, 'You approved additional spending for today, but there is not enough balance for `spend` to occur');
+  //     dailyLimitConcrete.add(amount);
+  //     event AdditionalSpendingForTodayApproved(address childAddress, uint256 amount);
+  // }
   function spend(uint256 amount, address destination) public isChild, isParent {
+      // if()
       uint256 balance = tokenInstance.balanceOf(address(this));
       require(balance >= amount , 'Not enough balance');
       require(spentSoFar <= dailyLimitConcrete, 'Approval of additional resources should be requested');
+      spentSoFar.add(amount);
       bool success = tokenInstance.safeTransfer(destination, balance);
       if(success){
         emit SpendingHappend(address(this), destination, balance);
       }
-      else revert;
+      else revert;    
+  }
+    function spend(uint256 amount, address destination) public isChild, isParent {
+
   }
 
 }
